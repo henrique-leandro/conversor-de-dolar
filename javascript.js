fa36e78a04c5f9cf9a901565bd87c6fb759f2076
henrique-leandro <henrique.leandro.sales@gmail.com>
2021-07-16 14:56:48 -0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+function conversordolar(){
+
+
+    let real = document.getElementById("input-real").value
+    let dolar = document.getElementById("input-dolar").value
+    
+    let resultado =  "R$ " + real / dolar + " dolares "
+    alert(resultado)
+}
